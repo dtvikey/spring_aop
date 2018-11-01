5098c27a98cf53102b73fef78ae194441415897f
dtvikey <dtvikey@qq.com>
2018-11-01 22:11:30 +0800

    v1.1

--- src/main/java/com/imooc/aop/demo1/SpringDemo1.java
+++ src/main/java/com/imooc/aop/demo1/SpringDemo1.java
@@ -6,9 +6,11 @@ public class SpringDemo1 {
     @Test
     public void demo1(){
         UserDao userDao = new UserDaoImpl();
-        userDao.save();
-        userDao.update();
-        userDao.delete();
-        userDao.find();
+        UserDao proxy = (UserDao) new MyJdkProxy(userDao).createProxy();
+
+        proxy.save();
+        proxy.update();
+        proxy.delete();
+        proxy.find();
     }
 }
