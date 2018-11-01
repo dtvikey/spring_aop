b196872fdf40ef538471009c8022d72872b0a60b
dtvikey <dtvikey@qq.com>
2018-11-01 21:54:31 +0800

    v1.1

--- src/main/java/com/imooc/aop/demo1/SpringDemo1.java
+++ src/main/java/com/imooc/aop/demo1/SpringDemo1.java
@@ -5,6 +5,10 @@ import org.junit.Test;
 public class SpringDemo1 {
     @Test
     public void demo1(){
-
+        UserDao userDao = new UserDaoImpl();
+        userDao.save();
+        userDao.update();
+        userDao.delete();
+        userDao.find();
     }
 }
