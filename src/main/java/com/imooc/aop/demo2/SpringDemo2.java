2dbe5bf2ede913adef2deba93de2d95fb6fcf388
dtvikey <dtvikey@qq.com>
2018-11-02 09:17:07 +0800

    v2.0

--- src/main/java/com/imooc/aop/demo2/SpringDemo2.java
+++ src/main/java/com/imooc/aop/demo2/SpringDemo2.java
@@ -7,10 +7,13 @@ public class SpringDemo2 {
     public void demo1(){
 
         ProductDao productDao = new ProductDao();
-        productDao.save();
-        productDao.update();
-        productDao.delete();
-        productDao.find();
+
+        ProductDao proxy = (ProductDao) new MyCglibProxy(productDao).createProxy();
+
+        proxy.save();
+        proxy.update();
+        proxy.delete();
+        proxy.find();
 
     }
 }
